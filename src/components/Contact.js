64e29866c3b5d2ab527bd5f23e412f0b9112fdd1
Rakesh Kumawat <rakeshkumawatrrkk12877@gmail.com>
2022-06-13 12:19:00 +0530

    added backend as firebase

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,23 +1,54 @@
 import React from 'react'
+import { db } from '../firebaseConfig'
+import { useState } from 'react'
+import { addDoc, collection } from 'firebase/firestore'
 
 export const Contact = () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [message, setMessage] = useState();
+
+  const userCollectionRef = collection(db, "contactdata")
+
+  const handleSubmit = () => {
+    addDoc(userCollectionRef, {
+      name: name, 
+      email: email,
+      message: message
+    }).then(()=>{
+      if (!alert("Form Submitted Successfully!!!")) document.location = "https://www.google.com"
+    }).catch(()=>{
+      alert()
+    })
+  }
+
   return (
     <div className='outerContainer'>
-        <p>Contact Form</p>
+      <p>Contact Form</p>
 
-        <form className='contactForm'>
-            <label>Full name</label>
-            <input type='text' placeholder="Enter your Full Name"/>
+      <form className='contactForm'>
+        <label>Full name</label>
+        <input type='text' placeholder="Enter your Full Name"
+          onChange={(event) => {
+            setName(event.target.value);
+          }} />
 
-            <label>Email Address</label>
-            <input type='text' placeholder="Enter your Email"/>
+        <label>Email Address</label>
+        <input type='text' placeholder="Enter your Email"
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }} />
 
-            <label>Enter Feedback</label>
-            <textarea type='text' placeholder="Enter your Message"></textarea>
+        <label>Enter Feedback</label>
+        <textarea type='text' placeholder="Enter your Message"
+          onChange={(event) => {
+            setMessage(event.target.value);
+          }} ></textarea>
 
-            <button>Submit</button>
 
-        </form>
+        <button onClick={handleSubmit}>Submit</button>
+
+      </form>
     </div>
   )
 }
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBf7ouw48KW7rwlphEAAdVD4wFZi5JaFP0",
-  authDomain: "contactform-16c0c.firebaseapp.com",
-  projectId: "contactform-16c0c",
-  storageBucket: "contactform-16c0c.appspot.com",
-  messagingSenderId: "589093329030",
-  appId: "1:589093329030:web:13fc5d79cb104af1679907",
-  measurementId: "G-89YSF5KN7Q"
+  apiKey: process.env.REACT_APP_FIREBASE_KEY ,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
